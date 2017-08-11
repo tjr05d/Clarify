e4e37d7d3bda35048ce2fb5ec7aee42d6c3d7330
tjr05d <tjr05d@my.fsu.edu>
2017-08-10 23:35:54 -0400

    adds some placeholder html to the DOM

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
 //adds message listener for browser action click
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
     if (request.message === "clicked_browser_action") {
+      let url = window.location.href
       let selection = getSelectedText(window);
-      alert(selection);
-      chrome.runtime.sendMessage({ "message": "new_clarity_request", "data": selection });
+      chrome.runtime.sendMessage({ "message": "new_clarity_request", "data": selection, "url": url });
+      addResponseDiv(document, selection)
     }
   }
 );
@@ -17,4 +18,43 @@ const getSelectedText = (window) => {
     text = document.selection.createRange().text
   } 
   return text
+}
+
+const addResponseDiv = (document, selection) => {
+  document.documentElement.style.height = '100%'
+  document.body.style.height = '100%'
+  document.documentElement.style.width = '100%'
+  document.body.style.width = '100%'
+
+  let div = document.createElement('div')
+
+  //append all elements
+  document.body.appendChild(div)
+  //set attributes for div
+  div.id = 'container'
+  div.style.position = 'fixed'
+  div.style.bottom = '0'
+  div.style.right = '0'
+  div.style.zIndex = '999999'
+  div.style.width = '30%'
+  div.style.height = '100%'
+  div.style.border = '2px solid gray'
+  div.style.backgroundColor = 'white'
+
+  AddTextToClarifyDiv('Clarify', div, '#98ff98', '50px')
+  AddTextToClarifyDiv(selection, div, 'white')
+}
+
+const AddTextToClarifyDiv = (text, parentDiv, color, height) =>{
+  let promptDiv = document.createElement('div')
+  promptDiv.id = 'prompt'
+  promptDiv.style.width = '100%'
+  promptDiv.style.height = height
+  promptDiv.style.backgroundColor = color
+  promptDiv.style.display = 'flex'
+  promptDiv.style.justifyContent = 'center'
+  promptDiv.style.padding = '18px'
+  promptDiv.style.borderBottom = '1px solid lightgrey'
+  promptDiv.innerText = text
+  parentDiv.appendChild(promptDiv)
 }
